d4e9e0160c7d9b533f5a56714820bffe19c6d471
Ramon Pawlowski <ramonpaw@gmail.com>
2020-07-03 13:24:42 +0200

    Section One
    - Added CSS
    - Added Icons, Background & Tiles

--- src/App.vue
+++ src/App.vue
@@ -8,34 +8,118 @@
 </template>
 
 <style>
-:root {
-  /*
+  :root {
+    /*
       NEW
     */
-  --backgroundColor: #adb5bd;
-  --darkColor: #212529;
-  --lightColor: #495057;
-  /*
+    --backgroundColor: #adb5bd;
+    --darkColor: #212529;
+    --lightColor: #535558;
+    /*
       OLD
     */
-  --primaryColor: #fff;
+    --primaryColor: #fff;
 
-  --alertColor: #f2dede;
-  --alertTextColor: #a94442;
+    --alertColor: #f2dede;
+    --alertTextColor: #a94442;
 
-  --borderColor: #eaecef;
-  --borderStrength: 1px;
-}
+    --borderColor: #eaecef;
+    --borderStrength: 1px;
+  }
 
-html,
-body,
-#app {
-  @apply w-full;
-  @apply h-full;
+  html,
+  body,
+  #app {
+    @apply w-full;
+    @apply h-full;
 
-  color: #626262;
-  background-color: transparent;
-}
+    color: #626262;
+    background-color: transparent;
+  }
+
+  .unselectable {
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+
+  /*
+  Button
+*/
+
+  button:active,
+  button:focus,
+  button {
+    outline: 0px !important;
+  }
+
+  button#highlighted,
+  button#default {
+    @apply text-center;
+    @apply py-1;
+    @apply px-2;
+    @apply rounded-sm;
+    @apply text-white;
+    transition: background-color 0.2s linear;
+  }
+
+  button#default {
+    @apply w-full;
+    @apply border-b;
+    border-color: transparent;
+
+    background-color: rgba(0, 0, 0, .65);
+    transition: border 0.2s linear;
+
+  }
+
+  button#highlighted {
+    animation-name: highlight;
+    animation-duration: 3s;
+    animation-delay: 1s;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+
+    transition: border-width 0.2s linear;
+  }
+
+  button#default:hover {
+    @apply border-white;
+  }
+
+  button#default:active {
+    @apply border-gray-500;
+  }
+
+  button#default+button#default {
+    @apply mx-1;
+  }
+
+  .imageCanvas {
+    clip-path: url("./assets/shapes/imageCanvas.svg#path");
+  }
+
+  /*
+    Animations
+  */
+  @keyframes highlight {
+    0% {
+      border-width: 1px;
+      border-color: transparent;
+    }
+
+    50% {
+      border-color: white;
+    }
+
+    100% {
+      border-width: 1px;
+      border-color: transparent;
+    }
+  }
 </style>
 <!--
   Color Palette
@@ -46,21 +130,21 @@ body,
 -->
 
 <script>
-import tplApp from "@/templates/views/tplApp.vue";
+  import tplApp from "@/templates/views/tplApp.vue";
 
-export default {
-  components: {
-    tplApp
-  },
-  data() {
-    return {
-      title: "Azrael Esports",
-      copyright: "Azrael Esports © 2020",
-      credits: "Ramon Pawlowski"
-    };
-  },
-  mounted() {
-    document.title = this.title;
-  }
-};
+  export default {
+    components: {
+      tplApp
+    },
+    data() {
+      return {
+        title: "Azrael Esports",
+        copyright: "Azrael Esports © 2020",
+        credits: "Ramon Pawlowski"
+      };
+    },
+    mounted() {
+      document.title = this.title;
+    }
+  };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,56 @@
 <template>
   <div id="home">
     <div id="section" class="first">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus
-      architecto error sit, dolorum aliquid sed, quas, modi harum in nemo atque
-      ipsam consequuntur magni minima? Ipsam veniam esse ipsa suscipit.
+      <div class="flex mx-5" style="height: 25%;">
+
+        <div class="imageCanvas">
+          <div id="title" class="text-2xl p-2 text-white border-b border-gray-600"
+            style="background-color: var(--darkColor); opacity: 0.65;">
+            <p>APEX LEGENDS</p>
+          </div>
+          <div style="background-color: rgba(0,0,0,.25); color: grey;">
+            <img style="height: 160px; width: 100%;" src="../assets/logo/apex-globalseries.jpg" alt="apex-globalseries">
+
+            <p class="p-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam illo vel harum iure
+              reiciendis, sequi
+              debitis natus deserunt repellat cumque, error similique quae impedit asperiores pariatur possimus nihil
+              ea. Necessitatibus.</p>
+          </div>
+          <button id="default">
+            MORE
+          </button>
+        </div>
+
+        <div class="imageCanvas">
+          <img src="../assets/logo/azrael-logoX512.png" alt="azrael-Logo">
+
+          <button id="highlighted" class="w-full mt-6 shadow-inner">
+            <p class="text-center text-4xl">
+              We want <strong>YOU !</strong>
+            </p>
+          </button>
+        </div>
+
+        <div class="imageCanvas">
+          <div id="title" class="text-2xl p-2 text-white border-b border-gray-600"
+            style="background-color: var(--darkColor); opacity: 0.65;">
+            <p>VALORANT</p>
+          </div>
+          <div style="background-color: rgba(0,0,0,.25); color: grey;">
+            <img class="pt-3" style="height: 160px; width: 100%;" src="../assets/icons/ico_plan.svg"
+              alt="apex-globalseries">
+
+            <p class="p-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam illo vel harum iure
+              reiciendis, sequi
+              debitis natus deserunt repellat cumque, error similique quae impedit asperiores pariatur possimus nihil
+              ea. Necessitatibus.
+            </p>
+          </div>
+          <button id="default">
+            MORE
+          </button>
+        </div>
+      </div>
     </div>
     <div id="section" class="second">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora ab ipsam
@@ -27,125 +74,143 @@
 </template>
 
 <style scoped>
-#home,
-#section {
-  height: 100vh;
-  width: 100%;
-}
+  #home,
+  #section {
+    height: 100vh;
+    width: 100%;
+  }
 
-#home {
-  max-height: 100vh;
-  overflow-y: scroll;
-  border: 1px solid gray;
-  scroll-snap-type: mandatory;
-  scroll-snap-points-y: repeat(3rem);
-  scroll-snap-type: y mandatory;
-}
+  #home {
+    max-height: 100vh;
+    overflow-y: scroll;
+    border: 1px solid gray;
+    scroll-snap-type: mandatory;
+    scroll-snap-points-y: repeat(3rem);
+    scroll-snap-type: y mandatory;
+  }
 
-#section {
-  scroll-snap-align: start;
-}
+  #section {
+    scroll-snap-align: start;
+  }
 
-#section:nth-child(0) {
-  @apply bg-gray-800;
-}
+  #section:nth-child(0) {
+    @apply bg-gray-800;
+  }
 
-#section:nth-child(1) {
-  @apply bg-gray-700;
-}
+  #section:nth-child(1) {
+    @apply bg-gray-700;
+  }
 
-#section:nth-child(2) {
-  @apply bg-gray-600;
-}
+  #section:nth-child(2) {
+    @apply bg-gray-600;
+  }
 
-#section:nth-child(3) {
-  @apply bg-gray-500;
-}
+  #section:nth-child(3) {
+    @apply bg-gray-500;
+  }
 
-#section:nth-child(4) {
-  @apply bg-gray-400;
-}
+  #section:nth-child(4) {
+    @apply bg-gray-400;
+  }
+
+  /*
+  First Section
+*/
+  #section.first {
+    background-image: url("../assets/background/tile_darkgraph.png");
+    background-size: auto;
+    background-position: left;
+    background-repeat: repeat;
+  }
+
+  #section .imageCanvas {
+    @apply w-1/3;
+    @apply mx-16;
+    padding-top: 15vh;
+    height: 100vh;
+  }
+
+  #section .imageCanvas:nth-of-type(1) {
+    color: black;
+  }
+
+  #section .imageCanvas:nth-of-type(2) {}
+
+  #section .imageCanvas:nth-of-type(3) {
+    color: black;
+  }
 </style>
 
 <script>
-export default {
-  name: "Home",
-  data() {
-    return {
-      page: 0,
-      windowTop: 0
-    };
-  },
-  mounted() {
-    this.onScroll();
-  },
-  methods: {
-    onScroll() {
-      var observerOne = new IntersectionObserver(
-        function(entries) {
-          entries.forEach(entry => {
-            const { isIntersecting } = entry;
-            if (isIntersecting === true) {
-              this.page = 1;
-              console.log(this.page);
+  export default {
+    name: "Home",
+    data() {
+      return {
+        page: 0
+      };
+    },
+    mounted() {
+      this.onScroll();
+    },
+    methods: {
+      onScroll() {
+        var observerOne = new IntersectionObserver(
+          function (entries) {
+            entries.forEach(entry => {
+              const { isIntersecting } = entry;
+              if (isIntersecting === true) {
+                this.page = 1;
+                console.log(this.page);
+              }
+            });
+          },
+          { threshold: 0.1 }
+        );
 
-              console.log("Passedby One");
-            }
-          });
-        },
-        { threshold: 0.1 }
-      );
+        var observerTwo = new IntersectionObserver(
+          function (entries) {
+            entries.forEach(entry => {
+              const { isIntersecting } = entry;
+              if (isIntersecting === true) {
+                this.page = 2;
+                console.log(this.page);
+              }
+            });
+          },
+          { threshold: 0.1 }
+        );
 
-      var observerTwo = new IntersectionObserver(
-        function(entries) {
-          entries.forEach(entry => {
-            const { isIntersecting } = entry;
-            if (isIntersecting === true) {
-              this.page = 2;
-              console.log(this.page);
+        var observerThree = new IntersectionObserver(
+          function (entries) {
+            entries.forEach(entry => {
+              const { isIntersecting } = entry;
+              if (isIntersecting === true) {
+                this.page = 3;
+                console.log(this.page);
+              }
+            });
+          },
+          { threshold: 0.1 }
+        );
 
-              console.log("Passedby Two");
-            }
-          });
-        },
-        { threshold: 0.1 }
-      );
+        var observerFour = new IntersectionObserver(
+          function (entries) {
+            entries.forEach(entry => {
+              const { isIntersecting } = entry;
+              if (isIntersecting === true) {
+                this.page = 4;
+                console.log(this.page);
+              }
+            });
+          },
+          { threshold: 0.1 }
+        );
 
-      var observerThree = new IntersectionObserver(
-        function(entries) {
-          entries.forEach(entry => {
-            const { isIntersecting } = entry;
-            if (isIntersecting === true) {
-              this.page = 3;
-              console.log(this.page);
-
-              console.log("Passedby Three");
-            }
-          });
-        },
-        { threshold: 0.1 }
-      );
-
-      var observerFour = new IntersectionObserver(
-        function(entries) {
-          entries.forEach(entry => {
-            const { isIntersecting } = entry;
-            if (isIntersecting === true) {
-              this.page = 4;
-              console.log(this.page);
-
-              console.log("Passedby Four");
-            }
-          });
-        },
-        { threshold: 0.1 }
-      );
-
-      observerOne.observe(document.querySelector(".first"));
-      observerTwo.observe(document.querySelector(".second"));
-      observerThree.observe(document.querySelector(".three"));
-      observerFour.observe(document.querySelector(".four"));
+        observerOne.observe(document.querySelector(".first"));
+        observerTwo.observe(document.querySelector(".second"));
+        observerThree.observe(document.querySelector(".three"));
+        observerFour.observe(document.querySelector(".four"));
+      }
     }
-  }
-};
+  };
 </script>
