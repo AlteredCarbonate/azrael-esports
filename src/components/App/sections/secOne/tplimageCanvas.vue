36fd4be9719d3c8d56be9881ec16cce8791a5dc5
Ramon Pawlowski <ramonpaw@gmail.com>
2020-07-11 19:15:00 +0200

    Template imageCanvas

--- src/components/App/sections/secOne/tplimageCanvas.vue
+++ src/components/App/sections/secOne/tplimageCanvas.vue
@@ -5,26 +5,15 @@
       class="text-2xl p-2 text-white border-b border-gray-600"
       style="background-color: var(--darkColor); opacity: 0.65;"
     >
-      <p><slot name="title"></slot></p>
+      <p><slot name="title" /></p>
     </div>
     <div style="background-color: var(--lightColor); color:white;">
       <div class="bg-white">
-        <slot name="img"></slot>
-        <!-- <apexglobalseries
-          class="w-full p-4"
-          color="black"
-          detail="#a4adb0"
-          style="height: 160px;"
-        /> -->
+        <slot name="img" />
       </div>
 
       <p class="p-2">
-        <slot name="desc">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam illo
-          vel harum iure reiciendis, sequi debitis natus deserunt repellat
-          cumque, error similique quae impedit asperiores pariatur possimus
-          nihil ea. Necessitatibus.
-        </slot>
+        <slot name="desc" />
       </p>
     </div>
     <button id="default" class="w-full">
